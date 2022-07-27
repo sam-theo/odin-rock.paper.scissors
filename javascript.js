353c1632d6f08dbb9b3fcd7aa1d859f38da17146
samh <samhaaker12@gmail.com>
2022-07-27 20:11:52 +0200

    added the main game playing code

--- javascript.js
+++ javascript.js
@@ -1,6 +1,23 @@
 
+function play(player){
+    if(player.charAt(0) === "s"){
+        return check(getcomputerchoice(),"p", "r", "s");
+    }else if(player.charAt(0) === "p"){
+        return check(getcomputerchoice(),"r", "s", "p");
+    }else{
+        return check(getcomputerchoice(),"s", "p", "r");
+    }
+}
 
-
+function check(comp,win,loser,tie){
+    if(comp === win){
+        return "you win!";
+    }else if(comp === loser){
+        return "You loze.";
+    }else{
+        return "It's a tie.";
+    }
+}
 
 function getcomputerchoice(){
     let a = Math.floor(Math.random() * 3);
